2524e087927dc747ce98f2a8e0181668d74423d8
CG-U <cguntalan1999@gmail.com>
2024-06-16 22:29:34 +0800

    fix: Inventory mobile view box sizing

--- src/pages/components/AddToInventory/AddToInventory.tsx
+++ src/pages/components/AddToInventory/AddToInventory.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 export function AddToInventory() {
   return (
     <div className="flex flex-col items-center justify-center h-full">
-      <div className="flex items-center justify-center w-full h-full space-x-4 rounded-lg md:w-2/3 md:h-2/3 lg:w-1/3 lg:h-1/3 bg-base-100 drop-shadow-lg">
+      <div className="flex items-center justify-center w-full h-full px-4 space-x-4 rounded-lg min-w-fit md:w-2/3 md:h-2/3 lg:w-1/3 lg:h-1/3 bg-base-100 drop-shadow-lg">
         <p className="font-bold">Choose Action:</p>
         <Link to="create-food" className="btn btn-outline btn-primary">
           Add Food To Menu
